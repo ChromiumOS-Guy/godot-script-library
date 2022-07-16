dc9d9369a4a90b9d93707ad7a0767fa643473fcc
ChromiumOS-Guy <57168079+ChromiumOS-Guy@users.noreply.github.com>
2022-07-16 18:13:24 +0300

    Update SceneChanger.gd

--- GDscript/autoload(singleton)/SceneChanger.gd
+++ GDscript/autoload(singleton)/SceneChanger.gd
@@ -5,11 +5,12 @@
 
 extends Node
 export var max_load_time = 10000
-var thread = Thread.new() # makes new thread 
+var thread = Thread.new() # makes new thread
+var can_async:bool = ["Windows", "OSX", "UWP", "X11"].has(OS.get_name())
 
-func start_load(path,doloadingbar = true): # example use: SceneChanger.start_load("res://levels/level1.tscn",self)
-	if Globals.can_async:
-		thread.start(self,"goto_scene",[ResourceLoader.load_interactive(path), doloadingbar, path],thread.PRIORITY_HIGH) # starts new thread with HIGH Priority
+func start_load(path): # example use: SceneChanger.start_load("res://levels/level1.tscn",self)
+	if can_async:
+		thread.start(self,"goto_scene",[ResourceLoader.load_interactive(path),path],thread.PRIORITY_HIGH) # starts new thread with HIGH Priority
 	else:
 		print("Error your platform does not support multi threading falling back to main thread")
 		get_tree().change_scene(path) # complete fall back 
@@ -17,18 +18,12 @@ func start_load(path,doloadingbar = true): # example use: SceneChanger.start_loa
 
 func goto_scene(data): # handels polling and scene switching
 	var loader = data[0]
-	var doloadingbar = data[1]
-	var path = data[2]
-	if Globals.can_async: # double check if platform supports aysnc (no fallback if it failes)
+	var path = data[1]
+	if can_async: # double check if platform supports aysnc (no fallback if it failes)
 		
 		if loader == null: # complains about invalid paths
 			print("Resource loader unable to load the resource at path")
 			return
-		
-		var loading_bar = Globals.loading_bar.instance() if doloadingbar else null# put your own loading screen here 
-		
-		if doloadingbar:
-			get_tree().get_root().call_deferred('add_child',loading_bar) # adding loading bar as child
 		
 		var t = OS.get_ticks_msec()
 		
@@ -36,13 +31,27 @@ func goto_scene(data): # handels polling and scene switching
 			var err = loader.poll()
 			if err == ERR_FILE_EOF:
 				#Loading Complete
-				var resource = loader.get_resource()
+				var resource = loader.get_resource().instance()
+				get_tree().get_root().add_child_below_node(get_tree().get_root(),resource)
 				for child in get_tree().get_root().get_children(): # gets children of root and queue_free() them if they are not a singleton
-					if !Globals.autoload_names.has(child.get_name()): # checks if the childs name is valid for exclusion from queue_free() by looking up its name and seeing if it exist in Globals.autoload_names
+					if Globals.autoload_names.has(child.get_name()): # checks if the childs name is valid for exclusion from queue_free() by looking up its name and seeing if it exist in Globals.autoload_names
+						pass
+					elif child == resource:
+						pass
+					else:
 						child.queue_free()
-				get_tree().get_root().call_deferred('add_child',resource.instance())
-				if !doloadingbar:
-					print(path.get_file()," is complete!")
+				print(path.get_file()," is complete!")
+				thread.wait_to_finish()
+				break
+			elif err == OK:
+				#Still loading
+				var progress = float(loader.get_stage())/loader.get_stage_count()
+				print(path.get_file()," is at %",progress * 100," completion!")
+			else:
+				print("Error while loading file") # if err isn't ERR_FILE_EOF(completed) or OK(still loading) the something Is preventing the file from loading
+				break
+			yield(get_tree(),"idle_frame") # makes it possible to see end result
+
 				thread.wait_to_finish()
 				break
 			elif err == OK:
